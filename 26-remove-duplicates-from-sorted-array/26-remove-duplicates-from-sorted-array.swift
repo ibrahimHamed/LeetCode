adc7f414ef221539c42645eaa517b773ebc5476f
Ibrahim Hamed <55464011+ibrahimHamed@users.noreply.github.com>
2022-08-26 15:59:48 +0200

    Time: 78 ms (59.98%), Space: 15 MB (41.78%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.swift
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.swift
@@ -1,5 +1,6 @@
 class Solution {
     func removeDuplicates(_ nums: inout [Int]) -> Int {
+        guard !nums.isEmpty else { return 0 }
         nums = Array(Set(nums)).sorted()
         return nums.count
     }
