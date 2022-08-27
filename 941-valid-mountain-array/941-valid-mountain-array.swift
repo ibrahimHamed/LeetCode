04db08c960cbdff20efd9d46a4557ebaef3ebfe8
Ibrahim Hamed <55464011+ibrahimHamed@users.noreply.github.com>
2022-08-27 23:42:38 +0200

    Time: 262 ms (53.57%), Space: 13.9 MB (99.11%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.swift
+++ 941-valid-mountain-array/941-valid-mountain-array.swift
@@ -1,30 +1,48 @@
 class Solution {
     func validMountainArray(_ arr: [Int]) -> Bool {
-        if arr.count < 3 || arr[0] > arr[1] { return false }
+        if arr.count < 3 { return false }
+        // [0,3,2,1]
         // if arr[0] > arr[1] { return false }
-        for i in 0..<arr.count - 1 {
-            if arr[i] == arr[i+1] { return false}
-        }
-        var flag = false
-        var indexOfTop = 0
-        for i in 0..<arr.count - 1 {
-            if arr[i] > arr[i+1] {
-                indexOfTop = i
-                break
-            } else {
-                flag = false
-            }
+        var index = 0
+        while index < arr.count - 1 && arr[index] < arr[index + 1]  {
+            index += 1
         }
         
-        for i in indexOfTop..<arr.count - 1 {
-            if arr[i] > arr[i + 1] {
-                flag = true
-            } else {
-                flag = false
-                break
-            }
+        if index == 0 || index == (arr.count - 1) { return  false }
+        3
+        while index < arr.count - 1 && arr[index] > arr[index + 1] {
+            index += 1
         }
-        return flag
+        
+        return index == (arr.count - 1)
+        
+        // [1,2,3,4,3,2,0]
+        // [1 , 2, , 2 , 3 , 0]
+        
+        
+//         for i in 0..<arr.count - 1 {
+//             if arr[i] == arr[i+1] { return false}
+//         }
+//         var flag = false
+//         var indexOfTop = 0
+//         for i in 0..<arr.count - 1 {
+//             if arr[i] > arr[i+1] {
+//                 indexOfTop = i
+//                 break
+//             } else {
+//                 flag = false
+//             }
+//         }
+        
+//         for i in indexOfTop..<arr.count - 1 {
+//             if arr[i] > arr[i + 1] {
+//                 flag = true
+//             } else {
+//                 flag = false
+//                 break
+//             }
+//         }
+//         return flag
         
     }
 }
