6497e392ca0324cd365f6efcd9863c0e6d414b4d
Ibrahim Hamed <55464011+ibrahimHamed@users.noreply.github.com>
2022-08-28 00:05:17 +0200

    Time: 233 ms, Memory: 14.3 MB - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.swift
@@ -0,0 +1,42 @@
+class Solution {
+    func validMountainArray(_ arr: [Int]) -> Bool {
+        if arr.count < 3 { return false }
+        var index = 0
+        while index < arr.count - 1 && arr[index] < arr[index + 1]  {
+            index += 1
+        }
+        
+        if index == 0 || index == (arr.count - 1) { return  false }
+        3
+        while index < arr.count - 1 && arr[index] > arr[index + 1] {
+            index += 1
+        }
+        
+        return index == (arr.count - 1)
+
+//         for i in 0..<arr.count - 1 {
+//             if arr[i] == arr[i+1] { return false}
+//         }
+//         var flag = false
+//         var indexOfTop = 0
+//         for i in 0..<arr.count - 1 {
+//             if arr[i] > arr[i+1] {
+//                 indexOfTop = i
+//                 break
+//             } else {
+//                 flag = false
+//             }
+//         }
+        
+//         for i in indexOfTop..<arr.count - 1 {
+//             if arr[i] > arr[i + 1] {
+//                 flag = true
+//             } else {
+//                 flag = false
+//                 break
+//             }
+//         }
+//         return flag
+        
+    }
+}
