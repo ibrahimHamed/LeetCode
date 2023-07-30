9ffb204e86daa7672335de35bb1ac56a9bbc655f
Ibrahim Hamed <55464011+ibrahimHamed@users.noreply.github.com>
2023-07-30 04:11:55 +0300

    Time: 3 ms (41.28%), Space: 6.4 MB (83.91%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int last_index = s.size() - 1;
-        for (int i = last_index; i >= 0; --i){
-            if (s[i] != ' '){
-                last_index = i;
-                break;
+        
+        // int last_index = s.size() - 1;
+        // for (int i = last_index; i >= 0; --i){
+        //     if (s[i] != ' '){
+        //         last_index = i;
+        //         break;
+        //     }
+
+
+        // }
+        // int i = last_index;
+        // while(i >= 0 && s[i] != ' '){
+        //     i--;
+        // }
+        // return last_index - i;
+
+        int n = s.size();
+        bool isStarted = false;
+        int ans = 0;
+        for (int i = n-1; i >= 0; i--)
+        {
+            if(!isStarted && s[i] != ' ')
+            {
+                isStarted = true;
+                ans = 1;
+            }
+            else if (isStarted)
+            {
+                if( s[i] == ' ') break;
+                else ans++;
             }
         }
-        int i = last_index;
-        while(i >= 0 && s[i] != ' '){
-            i--;
-        }
-        return last_index - i;
+        return ans;
     }
 };
